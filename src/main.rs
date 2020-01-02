9fd9bd961684294ee01bae6b29690460626e6fd6
Niketin <Niketin@users.noreply.github.com>
2020-01-03 01:58:29 +0200

    Add reading port number from env variable PORT

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use actix_web::{web, App, HttpServer};
+use std::env;
 
 mod common;
 mod routes;
 
 fn main() -> std::io::Result<()> {
+    let port: u16 = env::var("PORT")
+        .expect("PORT variable must be set")
+        .parse()
+        .expect("PORT must be a number");
+        
+
     HttpServer::new(|| {
         App::new()
             .data(common::AppData::default())
@@ -18,6 +25,6 @@ fn main() -> std::io::Result<()> {
                     .route(web::get().to(routes::package_handler)),
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", port))?
     .run()
 }
